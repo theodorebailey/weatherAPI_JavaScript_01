8c94eabd46aed334d75c3c4b5c9f3f3cdf8801e3
Theodore Bailey <tawbailey@hotmail.com>
2023-07-23 16:58:27 +0100

    working project, need to add city functionality for users

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
 const apiKey = "8bce3dfa224b828d214f42fa8c08110d";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=london";
 
-async function checkWeather() {
+// asynchronous function response is fetching an API URL 
+// 
+async function checkWeather(city) {
     const response = await fetch(apiURL + `&appid=${apiKey}`)
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
